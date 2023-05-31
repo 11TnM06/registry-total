79889ecbb2576ac77ec88548a18611544f00e6ef
20021386-NguyenDucHoangLong <pleasewaitlong@gmail.com>
2023-05-31 23:54:08 +0700

    Login + GetAll frontend
    - use api to get data from server

--- registry-total-frontend/src/utils/UseTranslator.js
+++ registry-total-frontend/src/utils/UseTranslator.js
@@ -20,13 +20,9 @@ const key = {
   technical: "Thông số kỹ thuật",
   username: "Tên đăng nhập",
   email: "Email",
-  role: "Chức vụ",
-  companyName: "Tên công ty",
   address: "Địa chỉ",
   phone: "Số điện thoại",
   option: "Tùy chọn",
-  numberOfErrorProduct: "Số lượng sản phẩm lỗi",
-  numberOfRestProduct: "Số lượng sản phẩm không có lỗi",
 };
 const UseTranslator = {};
 
